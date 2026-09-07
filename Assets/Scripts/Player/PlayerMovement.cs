04db9e055419e2ff63aade98eba824536c5e05ae
dshanzotoe-creator <darrenscotthanzotoe@gmail.com>
2026-09-07 21:51:45 +0200

    Add PlayerMovement and Playerstats

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : PlayerStats
 {
+    private Rigidbody2D _rb;
+
+    [SerializeField] InputAction _action;
+
+    private float _moveSpeedMultiplier = 1f; 
+
+    private void Awake()
+    {
+        movementSpeed = 5.0f; 
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private void FixedUpdate()
+    {
+        Movement(); 
+    }
+
+
+    private void Movement()
+    {
+        Vector2 _direction; 
+
+        _direction = _action.ReadValue<Vector2>();
+        _direction.Normalize();
+
+        _rb.linearVelocity = _direction * (movementSpeed * _moveSpeedMultiplier);
+    }
+
+
+
+    private void OnEnable()
+    {
+        _action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _action.Disable();
     }
 }
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    protected float movementSpeed;
+    
+    protected float damageModifier;
+
+    protected float health;
+
+    protected float shotSpeedModifier; 
+
+
+    
+
+}
